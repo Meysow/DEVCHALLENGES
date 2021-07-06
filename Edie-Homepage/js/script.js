046ddb1c8ee183c1f187b9fe3021c0c963166128
Meysow <78431327+Meysow@users.noreply.github.com>
2021-07-06 17:41:10 +0400

    Update script.js
    V2

--- Edie-Homepage/js/script.js
+++ Edie-Homepage/js/script.js
@@ -1,5 +1,7 @@
-const btn = document.querySelector('.btn')
-const hamburger = document.querySelector('.hamburger')
+// HAMBURGER MENU
+
+const btn = document.querySelector('.select__btn')
+const hamburger = document.querySelector('.select__ham')
 const menuLinks = document.querySelectorAll('.link')
 
 function toggleMenu() {
@@ -15,8 +17,40 @@ function toggleMenu() {
 
 btn.addEventListener('click', toggleMenu);
 
+function closeMenu() {
+  if (btn.classList.contains('active')) {
+    btn.classList.remove('active')
+    btn.classList.add('not-active')
+    hamburger.classList.remove('active')
+  } else {
+    return false;
+  }
+}
+
 menuLinks.forEach(
   function(menuLink) {
-    menuLink.addEventListener('click', toggleMenu);
+    menuLink.addEventListener('click', closeMenu);
   }
 )
+
+
+// SCROLL EFFECT 
+
+function scrollMenu() {
+  let y = window.scrollY > 100;
+  console.log(y)
+  
+  if (y == true) {
+    hamburger.classList.add('hamburger__scroll');
+    hamburger.classList.remove('hamburger');
+    btn.classList.add('btn__scroll');
+    btn.classList.remove('btn');
+  } else {
+    hamburger.classList.add('hamburger');
+    hamburger.classList.remove('hamburger__scroll');
+    btn.classList.add('btn');
+    btn.classList.remove('btn__scroll');
+  }
+}
+
+window.addEventListener('scroll', scrollMenu)
