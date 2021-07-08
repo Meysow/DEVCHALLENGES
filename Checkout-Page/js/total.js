34c59d2952b8912650b9a10a7754584871fb5d4e
Meysow <78431327+Meysow@users.noreply.github.com>
2021-07-08 19:24:58 +0400

    Update total.js

--- Checkout-Page/js/total.js
+++ Checkout-Page/js/total.js
@@ -1,17 +1,34 @@
 function CalculateItemsValue() {
-    var total = 0;
-    var total_items = 2;
-    var ship = parseInt(document.getElementById("shipping_cost").getAttribute("data-price"));
+    let total = 0;
+    let total_items = 2;
+    let shippingCost = parseInt(document.getElementById("shipping_cost").getAttribute("data-price"));
+
     for (i=1; i<=total_items; i++) {
-        
         itemID = document.getElementById("qnt_"+i);
-        nbID = document.getElementById("myNumber"+i)
+        nbID = document.getElementById("myNumber"+i);
+
         if (typeof itemID === 'undefined' || itemID === null) {
             alert("No such item - " + "qnt_"+i);
         } else {
             total = total + parseInt(nbID.value) * parseFloat(itemID.getAttribute("data-price"));
         }
-         
     }
-    document.getElementById("ItemsTotal").innerHTML = "$" + Math.round((total + ship + Number.EPSILON) * 100) / 100;
+
+    if (total == 0) {
+        document.getElementById("ItemsTotal").innerHTML = "$0";
+    } else {
+        document.getElementById("ItemsTotal").innerHTML = "$" + Math.round((total + shippingCost + Number.EPSILON) * 100) / 100;
+    }
+    document.getElementById('shipping_cost').innerHTML = "$" + shippingPrice();
+}
+
+function shippingPrice() {
+    let myNumber1 = document.getElementById('myNumber1');
+    let myNumber2 = document.getElementById('myNumber2');
+
+    if (parseInt(myNumber1.value) > 0 || parseInt(myNumber2.value) > 0) {
+        return 19;
+    } else {
+        return 0;
+    }
 }
